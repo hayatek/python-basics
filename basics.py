f48acf4d00057c1f909cd7e6d9237128baf8e7bd
hayatek <stream.hyt@gmail.com>
2022-02-13 07:52:04 +0900

    updated basics.py

--- basics.py
+++ basics.py
@@ -1,4 +1,15 @@
 #改行は\n、タブは\t
-#split関数はスペースと改行とタブが複数あってもそこで分割される
-s = "one two  three   \nfour \t"
-print(s.split())
+#split():はスペースと改行とタブが複数あってもそこで分割される
+#s = "one two  three   \nfour \t"
+#print(s.split())
+#one
+#two
+#three
+#four
+
+#strip()は両端の空白やタブを削除する
+input_s = "   200         300   "
+str = input_s.strip()
+print(str)
+#200 300
+
